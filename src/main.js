40a77a82ba8835cce4e7951ddc5d5c0a16535419
hooman mousavi <hoomanmousavi77@gmail.com>
2023-01-04 16:08:44 +0330

    initializing project

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//////////// Tailwind.config
+import './style/Tailwind.config/Tailwind.base.scss'
+import './style/Tailwind.config/Tailwind.component.scss'
+import './style/Tailwind.config/Tailwind.utilities.scss'
+
+
+////////// Vue
+import {createApp} from "vue";
+import App from "./App.vue";
+const app=createApp(App)
+app.mount('#app')
