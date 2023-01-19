40f64fce638ed7c08fd10a3ee94665a4fd05c086
hooman mousavi <hoomanmousavi77@gmail.com>
2023-01-19 21:57:36 +0330

    privacy policy page completed

--- src/pages/policy/privacy policy.vue
+++ src/pages/policy/privacy policy.vue
@@ -1,5 +1,92 @@
 <template>
-  privacy-policy
+  <section class="py-2" id="refund-policy">
+    <container>
+      <row class="justify-center">
+        <column col="12" md="6">
+          <h1 class="text-center font-700">Refund policy</h1>
+          <div class="my-1.5">
+            <p class="!text-1">This Privacy Policy describes how your personal information is collected, used, and shared when you visit or make a purchase from artofbrentos.myshopify.com (the “Site”). </p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">Personal information we collect</p>
+            <p class="!text-1">When you visit the Site, we automatically collect certain information about your device, including information about your web browser, IP address, time zone, and some of the cookies that are installed on your device. Additionally, as you browse the Site, we collect information about the individual web pages or products that you view, what websites or search terms referred you to the Site, and information about how you interact with the Site. We refer to this automatically-collected information as “Device Information”.  </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">We collect Device Information using the following technologies:
+              <br>
+              - “Cookies” are data files that are placed on your device or computer and often include an anonymous unique identifier. For more information about cookies, and how to disable cookies, visit http://www.allaboutcookies.org.
+              <br>
+              - “Log files” track actions occurring on the Site, and collect data including your IP address, browser type, Internet service provider, referring/exit pages, and date/time stamps.
+              <br>
+              - “Web beacons”, “tags”, and “pixels” are electronic files used to record information about how you browse the Site.  </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">
+              Additionally when you make a purchase or attempt to make a purchase through the Site, we collect certain information from you, including your name, billing address, shipping address, payment information (including credit card numbers, email address, and phone number. We refer to this information as “Order Information”.
+            </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">
+              When we talk about “Personal Information” in this Privacy Policy, we are talking both about Device Information and Order Information.
+            </p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">How do we use your personal information?</p>
+            <p class="!text-1">
+              We use the Order Information that we collect generally to fulfill any orders placed through the Site (including processing your payment information, arranging for shipping, and providing you with invoices and/or order confirmations). Additionally, we use this Order Information to:
+              <br>
+              - Communicate with you;
+              <br>
+              - Screen our orders for potential risk or fraud; and
+              <br>
+              - When in line with the preferences you have shared with us, provide you with information or advertising relating to our products or services.  </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">We use the Device Information that we collect to help us screen for potential risk and fraud (in particular, your IP address), and more generally to improve and optimize our Site (for example, by generating analytics about how our customers browse and interact with the Site, and to assess the success of our marketing and advertising campaigns). </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">We may make use of retarget advertising to present relevant forms of advertising to you.</p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">Sharing your personal Information</p>
+            <p class="!text-1">We share your Personal Information with third parties to help us use your Personal Information, as described above. For example, we use Shopify to power our online store--you can read more about how Shopify uses your Personal Information here: https://www.shopify.com/legal/privacy. We also use Google Analytics to help us understand how our customers use the Site -- you can read more about how Google uses your Personal Information here: https://www.google.com/intl/en/policies/privacy/. You can also opt-out of Google Analytics here: https://tools.google.com/dlpage/gaoptout.  </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">Finally, we may also share your Personal Information to comply with applicable laws and regulations, to respond to a subpoena, search warrant or other lawful request for information we receive, or to otherwise protect our rights.  </p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">Data retention</p>
+            <p class="!text-1">When you place an order through the Site, we will maintain your Order Information for our records unless and until you ask us to delete this information.
+            </p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">Changes</p>
+            <p class="!text-1">We may update this privacy policy from time to time in order to reflect, for example, changes to our practices or for other operational, legal or regulatory reasons.
+            </p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">Minors </p>
+            <p class="!text-1">The Site is not intended for individuals under the age of 18.
+            </p>
+          </div>
+          <div class="my-1.5">
+            <p class="font-700 !text-1">Contact us </p>
+            <p class="!text-1">For more information about our privacy practices, if you have questions, or if you would like to make a complaint, please contact us by e‑mail at hello@brentos.com.au or by mail using the details provided below:
+            </p>
+          </div>
+          <div class="my-1.5">
+            <p class="!text-1">Brentos
+              <br>
+              [Re: Privacy Compliance Officer]
+              <br>
+              241A HUDSON PARADE, CLAREVILLE NSW 2107, Australia
+            </p>
+          </div>
+
+        </column>
+      </row>
+    </container>
+  </section>
 </template>
 
 <script>
