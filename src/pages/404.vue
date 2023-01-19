63d6e1ede23281c72d9241f7c7fd89d18c11c65d
hooman mousavi <hoomanmousavi77@gmail.com>
2023-01-19 20:21:29 +0330

    404 page completed

--- src/pages/404.vue
+++ src/pages/404.vue
@@ -1,5 +1,17 @@
 <template>
-404
+  <section class="py-2" id="not-found-page">
+    <container>
+      <row>
+        <column col="12">
+            <h1 class="font-700 ">Page not found</h1>
+          <p class="mt-1 mb-3">The page you are looking for cannot be found</p>
+          <router-link class="btn btn-lg btn-dark-fill " :to="{name:'HOME'}">
+            Continue Browsing
+          </router-link>
+        </column>
+      </row>
+    </container>
+  </section>
 </template>
 
 <script>
